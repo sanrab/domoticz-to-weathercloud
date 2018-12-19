6aa6d4b27fdbcd84a76affe83627484f9cb3d2d1
sanrab <sanrab@users.noreply.github.com>
2018-12-19 14:57:44 +0100

    Update domo2wcloud_masked.php

--- domo2wcloud_masked.php
+++ domo2wcloud_masked.php
@@ -1,6 +1,5 @@
 #!/usr/bin/php
 
-
 <?php
 
 /*
@@ -9,18 +8,14 @@ sanrab, 19 dec 2018
 */
 
 $server_url = "http://api.weathercloud.net/v01/set";
-$wcloud_id = "XXXXXXX";
-$wcloud_key = "XXXXXXX";
-$login_url = "$server_url/wid/$wcloud_id/key/$wcloud_key";
+$wcloud_id = "weathercloud_id";
+$wcloud_key = "weathercloud_key";
 $domo_ip_port = "http://domoticz_ip:domoticz_port";
-$temp_file = "data.json";
-
+$ver="0.1";
+$type="4c60f6455101";
 
 $json_string = file_get_contents("$domo_ip_port/json.htm?type=devices&used=true&filter=all&favorite=1");
 $parsed_json = json_decode($json_string, true);
-$data = fopen ($temp_file, "w");
-fwrite ($data, print_R($parsed_json, TRUE));
-fclose ($data);
 
 //	parse variables
 
@@ -46,14 +41,15 @@ $parsed_json = json_decode($json_string, true);
 $parsed_json = $parsed_json['result'][3];
 $dir = $parsed_json['Direction'];
 
+// changes for weathercloud format
+
 $temp_ext=$temp_ext*10;
 $bar=$bar*10;
 $temp_int=$temp_int*10;
 $rain=$rain*10;
 $speed=$speed*10;
 
-$ver="0.1";
-$type="4c60f6455101";
+
 $part_preamble="$server_url/wid/$wcloud_id/key/$wcloud_key/ver/$ver/type/$type";
 $part_data="/temp/$temp_ext/temp_in/$temp_int/hum/$hum/bar/$bar/rain/$rain/wspd/$speed/wdir/$dir";
 
